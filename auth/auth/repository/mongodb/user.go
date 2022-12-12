af6525ebbdd4231bf696c04d5e2297039cc80778
socialthinks <socialthinks@socialthinkss-MacBook-Air.local>
2022-12-12 15:52:51 +0300

    just update

--- auth/auth/repository/mongodb/user.go
+++ auth/auth/repository/mongodb/user.go
@@ -2,10 +2,10 @@ package mongodb
 
 import (
 	"context"
+	"github.com/google/uuid"
 
 	"github.com/halilylm/ticketing/auth/domain"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,20 +20,18 @@ func NewUserRepository(collection *mongo.Collection) domain.UserRepository {
 
 // Insert creates a new user in mongodb
 func (u *userRepository) Insert(ctx context.Context, user *domain.User) (*domain.User, error) {
-	res, err := u.collection.InsertOne(ctx, user)
+	user.ID = uuid.NewString()
+	_, err := u.collection.InsertOne(ctx, user)
 	if err != nil {
 		return nil, err
 	}
-	uid, _ := res.InsertedID.(primitive.ObjectID)
-	user.ID = uid.Hex()
 	return user, nil
 }
 
 // FindByID finds a user by its id
 func (u *userRepository) FindByID(ctx context.Context, id string) (*domain.User, error) {
-	uid, _ := primitive.ObjectIDFromHex(id)
 	var foundUser domain.User
-	res := u.collection.FindOne(ctx, bson.M{"_id": uid})
+	res := u.collection.FindOne(ctx, bson.M{"_id": id})
 	if res.Err() != nil {
 		return nil, res.Err()
 	}
